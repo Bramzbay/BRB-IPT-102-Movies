7b7f8b5832c44c7eee1f1221550d0b0d95e9d427
Baybayon, Bramie R <baybayon.bramie.05141977@gmail.com>
2023-10-21 21:32:52 +0800

    Late Updated

--- BSIT3L-Movies/Models/MoviesModel.cs
+++ BSIT3L-Movies/Models/MoviesModel.cs
@@ -4,12 +4,15 @@ namespace BSIT3L_Movies.Models
     
 	public class MovieViewModel
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public string Rating { get; set; }
         public int ReleaseYear { get; set; }
         public string Genre { get; set; }
-        
+
+        public string Imagelnk { get; set; }
+
     }
 }
 
